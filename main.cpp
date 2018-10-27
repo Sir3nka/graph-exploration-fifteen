34e7c39726a78502f426ee609bb73916856a6924
Sir3nka <adriandugosz277@gmail.com>
2018-10-27 15:32:58 +0200

    Zabezpieczenia, parsowanie z argumentów, ogólne zabezpieczenia

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <map>
 #include <fstream>
 #include <sstream>
+#include <time.h>
+
 
 matrix Goal ={{1,2,3,4},
                   {5,6,7,8},
@@ -30,7 +32,7 @@ int BFS (std::shared_ptr<Board> BoardState, matrix resoult, std::vector<std::str
 {
     //TODO Kinda stuck on how to move around graph via nodes, should I save the state of the board before entering childrens?
     if(BoardState->getBoardSize()==Goal){
-        std::cout<<"Found Solution";
+        std::cout<<"Found Solution, loaded matrix is equal to final matrix";
         return 1;
     }
     auto start = std::make_shared<Node>("Error", nullptr, BoardState->getNeightbours(), BoardState->getBoardSize(), pattern);
@@ -38,42 +40,40 @@ int BFS (std::shared_ptr<Board> BoardState, matrix resoult, std::vector<std::str
     std::shared_ptr<Node>Children;
     std::set<matrix> hold;
     std::queue<std::shared_ptr<Node>> open_list;
-    std::map<matrix, int> explored;
+    std::unordered_set<matrix, VectorHash> explored;
 
     open_list.push(start);
-    int numExpanded = 0 ;
-    int a1=0;
-    int  a2=0;
     while(!open_list.empty()){
         curNode = open_list.front();
-        explored[curNode->getState()]=1;
+        explored.insert(curNode->getState());
         open_list.pop();
-            if(BoardState->getBoardSize()==Goal){
-                    std::cout << curNode->getCounter() << std::endl;
-                    std::cout << curNode->getPath() ;
+        if(BoardState->getBoardSize()==Goal){
+                std::cout << curNode->getCounter() << std::endl;
+                std::cout << curNode->getPath() ;
+                return 1;
+        }
+        else {
+            for (const auto &it:curNode->getNeightbours()){
+                BoardState->setBoardSize(curNode->getState());
+                BoardState->setCoordinates();
+                BoardState->takeAction(it);
+
+                Children = std::make_shared<Node> (it,curNode, BoardState->getNeightbours() , BoardState->getBoardSize(), pattern);
+                if(BoardState->getBoardSize()==Goal) {
+                    std::cout << Children->getCounter() << std::endl;
+                    std::cout << Children->getPath() ;
                     return 1;
-            }
-            else {
-                for (const auto &it:curNode->getNeightbours()){
-                    BoardState->setBoardSize(curNode->getState());
-                    BoardState->setCoordinates();
-                    BoardState->takeAction(it);
-
-                    Children = std::make_shared<Node> (it,curNode, BoardState->getNeightbours() , BoardState->getBoardSize(), pattern);
-                    if(BoardState->getBoardSize()==Goal) {
-                        std::cout << Children->getCounter() << std::endl;
-                        std::cout << Children->getPath() ;
-                        return 1;
-                    }
-                   if(explored.find(Children->getState())!=explored.end())
-                       continue;
-                       if(hold.find(Children->getState())!=hold.end())
-                           continue;
-                       open_list.push(Children);
-                       hold.insert(Children->getState());
-
                 }
+               if(explored.find(Children->getState())!=explored.end())
+                   continue;
+
+                   if(hold.find(Children->getState())!=hold.end())
+                       continue;
+                   open_list.push(Children);
+                   hold.insert(Children->getState());
+
             }
+        }
 
     }
     return 15;
@@ -82,43 +82,52 @@ int BFS (std::shared_ptr<Board> BoardState, matrix resoult, std::vector<std::str
 matrix parserToMatrix(std::string &arg) {
 matrix Numbers;
 std::ifstream infile(arg.c_str());
-std::string String;
-int a;
-    while(getline(infile, String)) {
+if(!infile.fail()) {
+    std::string String;
+    int a;
+    while (getline(infile, String)) {
         //Kinda lazy solution
         if (String.size() > 3) {
             std::vector<int> Line;
             std::stringstream Stream(String);
             while (Stream >> a)
-            Line.push_back(a);
+                Line.push_back(a);
             Numbers.push_back(Line);
         }
     }
     infile.close();
     return Numbers;
+    }
+    else std::cout << "FILE DOESNT EXIST!" << std::endl;
+    return Goal;
 }
-int main() {
-    std::vector<std::string>  fileName;
-  //  if(argc>1) {
-  //     //We need to assign second argument that we pass to our program becaus first is call to execute our file in this case ./FifteenGame
-  //      fileName.assign(argv + 1, argv + argc);
-  //  }
-    std::string Hold;
-    std::cin >> Hold;
-  //  for(auto &it:fileName)
-  //      Hold.append(it);
-    std::vector<std::string> pattern ;
+int main(int argc, char* argv[]) {
+    clock_t tStart = clock();
 
-    for (int i=0;i <=3; i++) {
-        std::string b;
-        std::cin >> b;
-        pattern.push_back(b);
+    std::vector<std::string>  fileName;
+    if(argc>1) {
+       //We need to assign second argument that we pass to our program becaus first is call to execute our file in this case ./FifteenGame
+        fileName.assign(argv + 1, argv + argc-1);
+    }
+    std::string Hold;
+    for(auto &it:fileName)
+        Hold.append(it);
+    std::vector<std::string> pattern ;
+    //STRASZNIE CHUJOWO ZROBIONE
+    if(argc>2) {
+        std::string str = (argv[2]);
+        std::string help;
+        for (unsigned int i = 0; i <= 3; i++) {
+            char hold = str.at(i);
+            pattern.push_back(help=hold);
+        }
     }
     matrix Testuje=parserToMatrix(Hold);
     auto Test = std::make_shared<Board>(Testuje);
     Test->setCoordinates();
 
     BFS(Test, Goal, pattern);
+    printf("\nTime taken: %.3fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
     return 0;
 }
